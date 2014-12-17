7febc06366acad3605fbfcd07266222a32d89ccd
Tim Buschmann <tim@buschmann-ge.de>
2014-12-17 10:57:49 +0100

    Init

---
+++ huffman.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   huffman.c
+ * Author: Tim
+ *
+ * Created on 17. Dezember 2014, 10:53
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+}
+
