d6d4b5ebd6c4f8b10352c642b26b0f8dfb0f702b
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-06 17:27:56 +0100

    + htree.h init
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

--- src/htree.h
+++ src/htree.h
@@ -11,6 +11,16 @@
 #define	HTREE_H
 
 
+typedef S_HTREE HTREE
+
+
+extern HTREE* create_htree(FREQTAB* freqtab);
+
+extern void delete_htree(HTREE* htree);
+
+extern CODETABLE_ELEMENT* htree_get_codetab_element(HTREE* htree);
+
+extern bool htree_is_emty(HTREE* htree);
 
 #endif	/* HTREE_H */
 
