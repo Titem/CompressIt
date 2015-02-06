3f8db068d8e5a5d7062fbde7f531b8508c691c16
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-06 17:01:20 +0100

    + codetab.h init
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

--- src/codetab.h
+++ src/codetab.h
@@ -11,6 +11,23 @@
 #define	CODETAB_H
 
 
+typedef CODETAB struct S_CODETAB
+
+
+
+extern CODETAB* create_codetab(htree HTREE*);
+
+extern CODETAB* read_codetab(FILE* input_stream);
+
+extern void wirte_codetab(FILE* output_stream, CODETAB* codetab);
+
+extern void delete_codetab(CODETAB* codetab);
+
+extern codetab_get_code(CODETAB* codetab, unsigned char character);
+
+extern codetab_get_char(CODETAB* codetab, unsigned short code);
+
+
 
 #endif	/* CODETAB_H */
 
