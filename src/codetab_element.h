9f700abde58ae1b5bf1b173b2f06c1dc1d015a91
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-06 17:18:16 +0100

    + codetab_element.h init
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

--- src/codetab_element.h
+++ src/codetab_element.h
@@ -11,6 +11,27 @@
 #define	CODETAB_ELEMENT_H
 
 
+typedef struct S_CODETAB_ELEMENT CODETAB_ELEMENT
+
+
+extern CODETAB_ELEMENT* creat_codetab_element(unsigned char character, 
+                                              unsigned short code,
+                                              unsigned char code_length);
+
+extern void delete_codetab_element(CODETAB_ELEMENT* codetab_element);
+
+extern bool codetab_element_is_bit_set(CODETAB_ELEMENT* codetab_element, 
+                                       unsigned char position);
+
+extern unsigned char codetab_element_get_char
+                     (CODETAB_ELEMENT* codetab_element);
+
+extern unsigned char codetab_element_get_code
+                     (CODETAB_ELEMENT* codetab_element);
+
+extern unsigned char codetab_element_get_code_length
+                     (CODETAB_ELEMENT* codetab_element);
+
 
 #endif	/* CODETAB_ELEMENT_H */
 
