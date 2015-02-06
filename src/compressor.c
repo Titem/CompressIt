00b507b212b767c45b5899d74fa2d02d8f15757c
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-06 20:25:18 +0100

    + Funktionsaufrufe repariert
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

--- src/compressor.c
+++ src/compressor.c
@@ -5,10 +5,10 @@ extern void compress(FILE* input_stream, FILE* output_stream)
 {
 	/*Häufigkeitstabelle erstellen.*/
 	FREQTAB* frequency_table = NULL;
-	frequency_table = create_frequency_table(input_stream);
+	frequency_table = create_freqtab(input_stream);
 
 	/*Häufigkeitstabelle auf dem Bildschirm ausgeben.*/
-	print_frequency_table(frequency_table);
+	freqtab_print(frequency_table);
 
 	/*Kompriemierungsvorgang einleiten.*/
 
