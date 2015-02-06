37ffb289cfab0963620f8eb2d8b3ac0bfbcc9145
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-06 16:43:07 +0100

    + main.h entfernt + Inhalt von main.h in main.c verschoben
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
-#include "main.h"
+#include "properties.h"
+#include "freqtab.h"
+#include <time.h>
 /* #define VisualStudio */
 #define DEBUG
+
+/*Jede C-Anwendung muss genau eine Main-Methode enthalten, in der angegeben ist, wo die Programmausführung beginnen soll.*/
+int main(int argc, char** argv);
+
+
 int main(int argc, char *argv[])
 {
 
--- src/main.h
+++
@@ -1,6 +0,0 @@
-#include "properties.h"
-#include "freqtab.h"
-#include <time.h>
-
-/*Jede C-Anwendung muss genau eine Main-Methode enthalten, in der angegeben ist, wo die Programmausführung beginnen soll.*/
-int main(int argc, char** argv);
