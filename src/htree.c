5f01183f21960f93e64ccb22265d940b5a398713
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-06 19:23:21 +0100

    + Funktionsrümpfe erstellt
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

--- src/htree.c
+++ src/htree.c
@@ -13,3 +13,23 @@ struct S_HTREE
     CODETAB_ELEMENT* prep_codetab_element;
     HTREE_ELEMENT* root_node;
 };
+
+extern HTREE* create_htree(FREQTAB* freqtab)
+{
+    
+}
+
+extern void delete_htree(HTREE* htree)
+{
+    
+}
+
+extern CODETABLE_ELEMENT* htree_get_codetab_element(HTREE* htree)
+{
+    
+}
+
+extern bool htree_is_emty(HTREE* htree)
+{
+    
+}
--- src/htree_element.c
+++ src/htree_element.c
@@ -27,3 +27,72 @@ struct S_HTREE_NODE
     HTREE_ELEMENT* right_child;
     unsigned long weight;
 };
+
+
+extern HTREE_ELEMENT* create_htree_node(HTREE_ELEMENT* left_child, 
+                                        HTREE_ELEMENT* right_child)
+{
+    
+}
+
+extern HTREE_ELEMENT* create_htree_leaf(unsigned long frequency, 
+                                        unsigned char character)
+{
+    
+}
+
+extern void delete_htree_element(HTREE_ELEMENT* htree_element)
+{
+    
+}
+
+extern HTREE_ELEMENT* merge_htree_elements(HTREE_ELEMENT* left_child, 
+                                           HTREE_ELEMENT* right_child)
+{
+    
+}
+
+extern bool htree_element_is_leaf(HTREE_ELEMENT* htree_element)
+{
+    
+}
+
+extern void htree_element_print(HTREE_ELEMENT* htree_element)
+{
+    
+}
+
+extern HTREE_ELEMENT* htree_node_get_left(HTREE_ELEMENT* htree_node)
+{
+    
+}
+
+extern HTREE_ELEMENT* htree_node_get_right(HTREE_ELEMENT* htree_node)
+{
+    
+}
+
+extern bool htree_node_has_left(HTREE_ELEMENT* htree_node)
+{
+    
+}
+
+extern bool htree_node_has_right(HTREE_ELEMENT* htree_node)
+{
+    
+}
+
+extern unsigned long htree_node_get_weight(HTREE_ELEMENT* htree_weight)
+{
+    
+}
+
+extern unsigned char htree_node_get_char(HTREE_ELEMENT* htree_leaf)
+{
+    
+}
+
+extern unsigned long htree_node_get_frequency(HTREE_ELEMENT* htree_leaf)
+{
+    
+}
