a982a5e6e5b03b7abddca4250e160c5c8fcd8a59
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-13 20:10:47 +0100

    pqueue.c falscher CAST korrigiert...
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

--- src/pqueue.c
+++ src/pqueue.c
@@ -124,12 +124,12 @@ static void pqueue_heapify(PQUEUE* pqueue, unsigned short index)
 
 static void pqueue_build_heap(PQUEUE* pqueue)
 {
-    unsigned short i;
+    short i;
     if (pqueue->number_of_entries > 0)
     {
-        for (i = ((pqueue->number_of_entries / 2) - 1); i >= 0; i--)
+        for (i = ((((short)pqueue->number_of_entries) / 2) - 1); i >= 0; i--)
         {
-            pqueue_heapify(pqueue, i);
+            pqueue_heapify(pqueue, (unsigned short)i);
         }
     }
 }
