a5c243f4807d01c90ee01ca90b8bbe0e50bf3cca
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-18 17:47:40 +0100

    Dokumentation codetab.h
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

--- src/codetab.h
+++ src/codetab.h
@@ -21,6 +21,11 @@
 /* ======================================================================== *
  * Typdefinitionen                                                          *
  * ======================================================================== */
+
+/**
+ * Dieser Datentyp repräsentiert eine Code-Tabelle.
+ * Variablen diesen Datentyps beinhalten Code-Tabellen. 
+ */
 typedef struct S_CODETAB CODETAB;
 
 
@@ -43,26 +48,115 @@ typedef struct S_CODETAB CODETAB;
  * Funktionsprototypen                                                      *
  * ======================================================================== */
 
+/**
+ * Diese Funktion erstellt eine neue Code-Tabelle auf dem Heap auf basis
+ * eines übergebenen Huffman-Baumes und liefert einen Zeiger auf die neu 
+ * erstellte Code-Tabelle zurück.
+ * 
+ * @param htree Zeiger auf Huffman-Baum auf dessen Basis die Code-Tabelle 
+ *              erstellt wird
+ * @return Zeiger auf neu erstellte Code-Tabelle
+ */
 extern CODETAB* create_codetab(HTREE* htree);
 
+
+
+/**
+ * Diese Funktion liest eine gespeicherte Code-Tabelle aus eine übergebenen 
+ * Eingangs-Datenstrom (z.b. aus einer Datei), legt diese im Heap an und 
+ * liefert einen Zeiger auf die neu angelegte Code-Tabelle zurück.
+ * Der übergebene Eingangs-Datenstrom muss an der Stelle stehen, an der die
+ * in der Datei gespeicherte Code-Tabelle beginnt.
+ * 
+ * @param input_stream Zeiger auf Datenstrom-Struktur, die die gespeicherte
+ *                     Code-Tabelle liefert
+ * @return Zeiger auf ausgelesene Code-Tabelle
+ */
 extern CODETAB* read_codetab(FILE* input_stream);
 
+
+
+/**
+ * Diese Funktion schreibt eine übergebene Code-Tabelle in einen übergeben
+ * Ausgangs-Datenstrom (z.B. in eine Datei).
+ * 
+ * @param output_stream Zeiger auf Datenstrom-Strukturm in die Code-Tabelle
+ *                      geschrieben werden soll
+ * @param codetab Zeiger auf Code-Tabelle die weggeschrieben werden soll
+ */
 extern void write_codetab(FILE* output_stream, CODETAB* codetab);
 
+
+
+/**
+ * Diese Funktion löscht eine übergebene Code-Tabelle vom Heap und gibt den
+ * Speicher wieder frei.
+ *  
+ * @param codetab Zeiger auf die Adresse der Code-Tabelle die gelöscht werden
+ *                soll
+ */
 extern void delete_codetab(CODETAB** codetab);
 
+
+
+/**
+ * Diese Funktion liefert den Code für das übergebene Zeichen.
+ * 
+ * @param codetab Zeiger auf die Code-Tabelle die abgefragt werden soll
+ * @param character Zeichen für das der passende Code geliefert werden soll
+ * @return Zeiger auf Code-Array, welches den Code für das übergebene
+ *         Zeichen enthält
+ */
 extern bool* codetab_get_code(CODETAB* codetab, 
                                unsigned char character);
 
-extern unsigned char codetab_get_code_length(CODETAB* codetab, 
-                                                unsigned char character);
 
+
+/**
+ * Diese Funktion liefert die Code-Länge für das übergebene Zeichen.
+ * 
+ * @param codetab Zeiger auf die Code-Tabelle die abgefragt werden soll
+ * @param character Zeichen für das die passende Code-Länge geliefert 
+ *                  werden soll
+ * @return positive Ganzzahl [0 bis 255] 
+ *         welche die Code-Länge für das übergebene Zeichen repräsentiert 
+ */
+extern unsigned char codetab_get_code_length(CODETAB* codetab, 
+                                             unsigned char character);
+
+
+
+/**
+ * Diese Funktion entnimmt ein Code-Tabellen-Element aus der Code-Tabelle
+ * und liefert einen Zeiger auf das entnommene Element zurück.
+ * 
+ * @param codetab Code-Tabelle der ein Element entnommen werden soll
+ * @return Zeiger auf entnommenes Code-Tabellen-Element
+ */
 extern CODETAB_ELEMENT* codetab_get_element(CODETAB* codetab);
 
+
+
+/**
+ * Diese Funktion liefert einen Wahrheitswert zurück, der angibt, ob die 
+ * übergebene Code-Tabelle noch Elemente enthält, oder leer ist.
+ * 
+ * @param codetab Zeiger auf die Code-Tabelle die abgefragt werden soll 
+ * @return Wahrheitswert, der gangibt ob die Code-Tabelle leer ist
+ *         (true = Code-Tabelle enthält KEINE Elemente (leer);
+ *          false = Code-Tabelle enthält noch Elemente)
+ */
 extern bool codetab_is_empty(CODETAB* codetab);
 
-extern void codetab_print(CODETAB* codetab);
 
+
+/**
+ * Diese Funktion gibt die übergebene Code-Tabelle auf dem Bildschirm aus.
+ * 
+ * @param codetab Zeiger auf Code-Tabelle, die auf dem Bildschirm ausgegeben 
+ *                werden soll
+ */
+extern void codetab_print(CODETAB* codetab);
 
 
 #endif	/* CODETAB_H */
