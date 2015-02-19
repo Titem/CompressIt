00a3308917335f4f464aa5ab28118c2bba4c011c
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-19 17:49:43 +0100

    common.h hinzugefügt...
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

---
+++ src/common.h
@@ -0,0 +1,20 @@
+/* 
+ * @file common.h
+ * 
+ * Dieses Modul enthält Definitionen, die von jedem Modul dieses Projektes
+ * benötigt werden.
+ * 
+ * @author Tim Buschmann
+ *         Artur Leinweber
+ *         Alexander Schmitz
+ */
+
+#ifndef COMMON_H
+#define	COMMON_H
+
+
+#define DEBUG_HUFFMAN
+
+
+#endif	/* COMMON_H */
+
