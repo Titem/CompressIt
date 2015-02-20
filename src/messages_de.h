5a726094277004e35d553d79998f9d5893475b33
Alexander Schmitz <info@alexander-schmitz.de>
2015-02-20 14:04:40 +0100

    messages_de.h init
    Signed-off-by: Alexander Schmitz <info@alexander-schmitz.de>

---
+++ src/messages_de.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   messages_de.h
+ * Author: Alexander Schmitz
+ *
+ * Created on 20. Februar 2015, 12:19
+ */
+
+#ifndef MESSAGES_DE_H
+#define	MESSAGES_DE_H
+
+#define MSG_CANT_ALLOCATE_MEMORY() printf("\
+        
+
+#endif	/* MESSAGES_DE_H */
+
